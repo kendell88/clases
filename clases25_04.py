22eac16c0c11ebea158e7ba2ce8d2b3e1bcb1718
kendell88 <ken.gutierrez@duocuc.cl>
2024-04-25 20:19:29 -0400

    Algunas cosas

---
+++ clases25_04.py
@@ -0,0 +1,52 @@
+# def  multi(num1, num2):
+#     print (num1*num2)
+
+# multi(100, 200)
+
+def suma(num1, num2):
+    result=num1+num2
+    return result
+num1=
+
+def multi( num1, num2 ):
+    resul=num1*num2
+    return resul
+num1=int(input())
+num2=int(input())
+print(multi(num1, num2))
+
+
+def resta(num1, num2):
+    result=num1-num2
+    return result
+num1=int(input())
+num2=int(input())
+print(resta(num1, num2))
+
+
+
+
+def division (num1, num2)
+    result=num1/num2
+    return result
+num1=int(input())
+num2=int(input())
+print(division(num1. num2))
+
+
+
+
+op=int(input())
+
+match op:
+    case 1:
+        print("opcion 1")
+    case 2:
+        print("opcion 2")
+    case 3:
+        print("opcion3")
+    case 4:
+        print("opcion 4")
+    case _:
+        print("opcion por defecto")
+
