d55dc460a2bd4486945fa27f230c0e741c569bc9
kendell88 <ken.gutierrez@duocuc.cl>
2024-04-27 14:05:31 -0400

    ccccc

---
+++ clasesabado.py
@@ -0,0 +1,28 @@
+
+
+# leche_sin_lactoso=True
+# caja_habilitada=True
+# sistema_de_pago=False
+
+
+
+
+
+
+
+# if lider=="abierto":
+#     print("puede entrar al super")
+#     if leche_sin_lactoso==True:
+#      if caja_habilitada==True:
+#       if sistema_de_pago==True:
+#        print("puede pagar con debito")
+
+#      else:
+#       print("puede pagar con efectivo")
+
+
+
+
+
+
+
